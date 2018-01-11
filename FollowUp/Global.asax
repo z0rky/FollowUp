d120db8cf291cabcfc00307442e63c9f1b0e38fc
Unknown <koenmeyers81@gmail.com>
2018-01-11 15:36:51 +0100

    nieuwe versie, navbar ok + lib

---
+++ FollowUp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FollowUp.MvcApplication" Language="C#" %>
---
+++ FollowUp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FollowUp.Startup))]
+namespace FollowUp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
