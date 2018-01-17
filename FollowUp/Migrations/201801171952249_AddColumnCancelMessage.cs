512f07aa2584820970979ce4322d64a0b70c5d93
Christophe Wolfs <christophe.wolfs@telenet.be>
2018-01-17 21:17:23 +0100

    Rebuild of database werkt nu

--- FollowUp/Migrations/201801171952249_AddColumnCancelMessage.cs
+++ FollowUp/Migrations/201801171952249_AddColumnCancelMessage.cs
@@ -8,11 +8,15 @@ namespace FollowUp.Migrations
         public override void Up()
         {
             AddColumn("dbo.Issues", "CancelMessage", c => c.String());
+            AddColumn("dbo.AspNetUsers", "RoleName", c => c.String());
+            AddColumn("dbo.AspNetUsers", "ManagerId", c => c.String());
         }
         
         public override void Down()
         {
             DropColumn("dbo.Issues", "CancelMessage");
+            DropColumn("dbo.AspNetUsers", "RoleName");
+            DropColumn("dbo.AspNetUsers", "ManagerId");
         }
     }
 }
